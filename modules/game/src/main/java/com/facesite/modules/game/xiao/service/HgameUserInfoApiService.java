3ee5725b496102d3cbf6ecf922420a8194a7ec1d
lihai <lihai1107>
2021-01-15 11:59:21 +0800

    增加游戏模块

--- modules/game/src/main/java/com/facesite/modules/game/xiao/service/HgameUserInfoApiService.java
+++ modules/game/src/main/java/com/facesite/modules/game/xiao/service/HgameUserInfoApiService.java
@@ -5,13 +5,11 @@ package com.facesite.modules.game.xiao.service;
 
 import cn.hutool.core.lang.Console;
 import com.alibaba.fastjson.JSONObject;
+import com.facesite.modules.game.xiao.dao.HgameInfoDao;
 import com.facesite.modules.game.xiao.dao.HgamePlayRecordDao;
 import com.facesite.modules.game.xiao.dao.HgameUserInfoDao;
 import com.facesite.modules.game.xiao.dao.HgameUserRefDao;
-import com.facesite.modules.game.xiao.entity.GameData;
-import com.facesite.modules.game.xiao.entity.HgamePlayRecord;
-import com.facesite.modules.game.xiao.entity.HgameUserInfo;
-import com.facesite.modules.game.xiao.entity.HgameUserRef;
+import com.facesite.modules.game.xiao.entity.*;
 import com.facesite.modules.game.xiao.utils.BaseGameContact;
 import com.facesite.modules.game.xiao.utils.DbGameContact;
 import com.facesite.modules.game.xiao.utils.HttpGameContact;
@@ -40,6 +38,8 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 	HgameUserRefDao hgameUserRefDao;
 	@Autowired
 	HgamePlayRecordDao hgamePlayRecordDao;
+	@Autowired
+	HgameInfoDao hgameInfoDao;
 
 	/**
 	 * @desc 游戏升级记录
@@ -59,13 +59,13 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 			String playId = gameData.getPlayId();
 			Integer type = DbGameContact.PLAY_TYPE_3;
 			synchronized (userId){
-				HgameUserRef oldGameUserRef = hgameUserRefDao.getByEntity(DbGameContact.getGameUserRefUser(userId,gameId));
+				HgameUserRef oldGameUserRef = hgameUserRefDao.getByEntity(DbGameContact.paramsGameUserRef(userId,gameId));
 				if(oldGameUserRef == null){
 					return BaseGameContact.failed("get user game info failed");
 				}
 				Long oldTotalScore = oldGameUserRef.getTotalScore();
 				//战绩重置
-				List<HgamePlayRecord>  list = hgamePlayRecordDao.findList(DbGameContact.uniqueRecord(type,userId,gameId,level));
+				List<HgamePlayRecord>  list = hgamePlayRecordDao.findList(DbGameContact.paramsGamePlayRecord(type,userId,gameId,level));
 				if(list !=null && list.size() > 0){
 					HgamePlayRecord oldRecord = list.get(0);
 					Boolean isOk = this.updateBestGameRecord(oldRecord,oldTotalScore,token,level,gold,score,start);
@@ -75,14 +75,14 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 
 				//app和本地乐豆同步
 				String tag = "游戏升级结算:"+gold;
-				Boolean isSync = this.synAppGold(token,userId,gameId,gold,tag);
+				Boolean isSync = this.syncAppGold(token,userId,gameId,gold,tag);
 				if(!isSync){
 					logger.error("app和本地乐豆同步失败:{}",isSync);
 					return BaseGameContact.failed("update app user gold failed");
 				}
 
 				//初始化战局
-				HgamePlayRecord newRecord = DbGameContact.initRecord(userId,gameId,playId,type,level,gold,score,start,tag);
+				HgamePlayRecord newRecord = DbGameContact.initGamePlayRecord(userId,gameId,playId,type,level,gold,score,start,tag);
 				Long dbIndex = hgamePlayRecordDao.insert(newRecord);
 				if(!BaseGameContact.isOkDb(dbIndex)){
 					return BaseGameContact.failed("save init game record failed");
@@ -123,7 +123,7 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 				oldRecord.setGold(gold);
 				Long newGold = gold - oldGold;
 				String tag = "游戏重玩结算:"+gold;
-				Boolean isSync = this.synAppGold(token,oldRecord.getUserId(),oldRecord.getGameId(),newGold,tag);
+				Boolean isSync = this.syncAppGold(token,oldRecord.getUserId(),oldRecord.getGameId(),newGold,tag);
 				if(!isSync){
 					logger.error("app和本地乐豆同步失败:{}",isSync);
 					return false;
@@ -159,20 +159,19 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 		}
 	}
 
-
 	/**
 	 * @desc app和本地乐豆同步
 	 * @author nada
 	 * @create 2021/1/15 10:05 上午
 	 */
 	@Transactional(readOnly=false)
-	public Boolean synAppGold(String token,String userId,String gameId, Long gold,String tag) {
+	public Boolean syncAppGold(String token,String userId,String gameId, Long gold,String tag) {
 		JSONObject result = HttpGameContact.updateAppGold(token,gold,tag);
 		Boolean isOk = BaseGameContact.isOk(result);
 		if(!isOk){
 			return false;
 		}
-		Long dbIndex = hgameUserRefDao.updateGameUserGold(DbGameContact.updateGameUserGold(userId,gameId,gold));
+		Long dbIndex = hgameUserRefDao.updateGameUserGold(DbGameContact.updateGameUserRefGold(userId,gameId,gold));
 		if(BaseGameContact.isOkDb(dbIndex)){
 			return true;
 		}
@@ -191,31 +190,17 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 			if(StringUtils.isEmpty(token)){
 				return BaseGameContact.failed("token parameter is empty");
 			}
+			List<HgameInfo> hgameInfoList = hgameInfoDao.findList(DbGameContact.paramsGameInfo(2));
+			if(hgameInfoList == null || hgameInfoList.size() < 1){
+				return BaseGameContact.failed("get gameinfo failed");
+			}
+			HgameInfo hgameInfo = hgameInfoList.get(0);
 			synchronized (token){
-				JSONObject result = HttpGameContact.getUserInfo(token);
-				Boolean isOk = BaseGameContact.isOk(result);
-				HgameUserInfo initUserInfo = null;
-				if(!isOk){
-					//初始化游客玩家
-					initUserInfo = DbGameContact.initVisitorUserInfo(token);
-				}else{
-					//会员玩家同步信息
-					JSONObject resData = result.getJSONObject("result");
-					HgameUserInfo params = DbGameContact.getUserInfoParent(resData.getString("userInfo_ID"));
-					HgameUserInfo hgameUserInfo = hgameUserInfoDao.getByEntity(params);
-					if(hgameUserInfo !=null){
-						String userId = hgameUserInfo.getId();
-						HgameUserRef hgameUserRef = this.getGameDb(userId);
-						return BaseGameContact.success(DbGameContact.getGameUserInfo(hgameUserInfo,hgameUserRef));
-					}
-					//初始化会员玩家
-					initUserInfo = DbGameContact.saveUserInfo(token,resData,DbGameContact.TYPE_MEMBER);
-				}
-				HgameUserRef hgameUserRef = this.initGameDb(initUserInfo);
+				HgameUserRef hgameUserRef = this.syncGetAppUserInfo(token,hgameInfo.getId());
 				if(hgameUserRef == null){
 					return BaseGameContact.failed("init userinfo failed");
 				}
-				return BaseGameContact.success(DbGameContact.getGameUserInfo(initUserInfo,hgameUserRef));
+				return BaseGameContact.success(DbGameContact.responseGameUserInfo(hgameInfo,hgameUserRef));
 			}
 		} catch (Exception e) {
 			Console.log("获取用户信息异常",e);
@@ -224,26 +209,42 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 	}
 
 	/**
-	 * @desc 获取用户游戏信息
+	 * @desc 同步获取app玩家信息
 	 * @author nada
-	 * @create 2021/1/12 7:31 下午
+	 * @create 2021/1/15 11:24 上午
 	*/
 	@Transactional(readOnly=false)
-	public HgameUserRef getGameDb(String userId){
+	public HgameUserRef syncGetAppUserInfo(String token,String gameId) {
 		try {
-			HgameUserRef userRefparams = DbGameContact.getGameUserRefUserId(userId);
-			HgameUserRef hgameUserRef = hgameUserRefDao.getByEntity(userRefparams);
-			if(hgameUserRef != null){
+			JSONObject result = HttpGameContact.getUserInfo(token);
+			Boolean isOk = BaseGameContact.isOk(result);
+			if(!isOk){
+				//初始化游客玩家
+				HgameUserInfo initUserInfo = DbGameContact.initVisitorUserInfo(token);
+				HgameUserRef hgameUserRef = this.initGameDb(initUserInfo,gameId);
 				return hgameUserRef;
 			}
-			hgameUserRef = DbGameContact.initGameUserRef("1",userId);
-			long db2 = hgameUserRefDao.insert(hgameUserRef);
-			if(!BaseGameContact.isOkDb(db2)){
-				return null;
+			JSONObject resData = result.getJSONObject("result");
+			String parentId = resData.getString("userInfo_ID");
+			HgameUserInfo hgameUserInfo = hgameUserInfoDao.getByEntity(DbGameContact.paramsGameUserInfo(parentId));
+			if(hgameUserInfo == null){
+				//初始化会员玩家
+				HgameUserInfo initUserInfo = DbGameContact.initMemberUserInfo(token,resData,DbGameContact.TYPE_MEMBER);
+				HgameUserRef hgameUserRef = this.initGameDb(initUserInfo,gameId);
+				return hgameUserRef;
+			}
+			String userId = hgameUserInfo.getId();
+			HgameUserRef hgameUserRef = hgameUserRefDao.getByEntity(DbGameContact.paramsGameUserRef(userId));
+			if(hgameUserRef == null){
+				hgameUserRef = DbGameContact.initGameUserRef(gameId,userId);
+				long db2 = hgameUserRefDao.insert(hgameUserRef);
+				if(!BaseGameContact.isOkDb(db2)){
+					return null;
+				}
 			}
 			return hgameUserRef;
 		} catch (Exception e) {
-			Console.log("获取用户游戏信息异常",e);
+			logger.error("同步获取app玩家信息异常",e);
 			return null;
 		}
 	}
@@ -254,14 +255,14 @@ public class HgameUserInfoApiService extends CrudService<HgameUserInfoDao, Hgame
 	 * @create 2021/1/12 7:25 下午
 	*/
 	@Transactional(readOnly=false)
-	public HgameUserRef initGameDb(HgameUserInfo initUserInfo){
+	public HgameUserRef initGameDb(HgameUserInfo initUserInfo,String gameId){
 		try {
 			long db1 = hgameUserInfoDao.insert(initUserInfo);
 			if(!BaseGameContact.isOkDb(db1)){
 				return null;
 			}
 			String userId = initUserInfo.getId();
-			HgameUserRef hgameUserRef = DbGameContact.initGameUserRef("1",userId);
+			HgameUserRef hgameUserRef = DbGameContact.initGameUserRef(gameId,userId);
 			long db2 = hgameUserRefDao.insert(hgameUserRef);
 			if(!BaseGameContact.isOkDb(db2)){
 				return null;
--- modules/game/src/main/java/com/facesite/modules/game/xiao/utils/DbGameContact.java
+++ modules/game/src/main/java/com/facesite/modules/game/xiao/utils/DbGameContact.java
@@ -1,10 +1,7 @@
 package com.facesite.modules.game.xiao.utils;
 
 import com.alibaba.fastjson.JSONObject;
-import com.facesite.modules.game.xiao.entity.GameData;
-import com.facesite.modules.game.xiao.entity.HgamePlayRecord;
-import com.facesite.modules.game.xiao.entity.HgameUserInfo;
-import com.facesite.modules.game.xiao.entity.HgameUserRef;
+import com.facesite.modules.game.xiao.entity.*;
 
 import java.util.UUID;
 
@@ -26,12 +23,37 @@ public class DbGameContact {
     public static int PLAY_TYPE_5 = 5;
     public static int PLAY_TYPE_6 = 6;
 
-    public static HgameUserInfo saveUserInfo(String token, JSONObject resData, int type){
+    /**
+     * @desc 响应游戏信息
+     * @author nada
+     * @create 2021/1/15 11:45 上午
+    */
+    public static JSONObject responseGameUserInfo(HgameInfo hgameInfo,HgameUserRef hgameUserRef){
+        JSONObject result = new JSONObject();
+        result.put("gid",hgameUserRef.getGameId());
+        result.put("uid",hgameUserRef.getUserId());
+        result.put("hbeans",hgameUserRef.getGold());
+        result.put("gold",hgameUserRef.getGold());
+        result.put("winGold",hgameInfo.getWin());
+        result.put("loseGold",hgameInfo.getLose());
+        result.put("totalScore",hgameUserRef.getTotalScore());
+        result.put("levelsCompleted", hgameUserRef.getLevelsCompleted());
+        result.put("boostersCount",hgameUserRef.getBoostersCount());
+        result.put("starsPerLevel",hgameUserRef.getStarsPerLevel());
+        return result;
+    }
+    /**
+     * @desc 初始化会员玩家
+     * @author nada
+     * @create 2021/1/15 11:26 上午
+     */
+    public static HgameUserInfo initMemberUserInfo(String token, JSONObject resData, int type){
         HgameUserInfo userInfo = new HgameUserInfo();
-        userInfo.setParentId(resData.getString("userInfo_ID"));
         userInfo.setType(type);
         userInfo.setToken(token);
         userInfo.setStatus("0");
+        userInfo.setAge(1);
+        userInfo.setParentId(resData.getString("userInfo_ID"));
         userInfo.setMobile(resData.getString("userInfo_Mobile"));
         userInfo.setNickname(resData.getString("userInfo_NickName"));
         userInfo.setUsername(resData.getString("userInfo_NickName"));
@@ -39,19 +61,22 @@ public class DbGameContact {
         userInfo.setLbeans(resData.getLong("userInfo_LBeans"));
         userInfo.setPhoto(resData.getString("userInfo_HeadImg"));
         userInfo.setSex(resData.getString("userInfo_Sex"));
-        userInfo.setAge(1);
         return userInfo;
     }
-
+    /**
+     * @desc 初始化游客玩家
+     * @author nada
+     * @create 2021/1/15 11:26 上午
+    */
     public static HgameUserInfo initVisitorUserInfo(String token){
         HgameUserInfo userInfo = new HgameUserInfo();
         userInfo.setParentId(UUID.randomUUID().toString());
         userInfo.setType(TYPE_VISITOR);
         userInfo.setToken(token);
         userInfo.setStatus("0");
-        userInfo.setMobile("");
-        userInfo.setNickname("");
-        userInfo.setUsername("");
+        userInfo.setMobile("123456");
+        userInfo.setNickname("游客");
+        userInfo.setUsername("游客");
         userInfo.setHbeans(0L);
         userInfo.setLbeans(0L);
         userInfo.setPhoto("");
@@ -59,28 +84,6 @@ public class DbGameContact {
         userInfo.setAge(1);
         return userInfo;
     }
-
-    public static JSONObject getGameUserInfo(HgameUserInfo userInfo, HgameUserRef hgameUserRef){
-        JSONObject result = new JSONObject();
-        result.put("gid",hgameUserRef.getGameId());
-        result.put("uid",hgameUserRef.getUserId());
-        result.put("type",userInfo.getType());
-        result.put("hbeans",userInfo.getHbeans());
-        result.put("levelsCompleted", hgameUserRef.getLevelsCompleted());
-        result.put("totalScore",hgameUserRef.getTotalScore());
-        result.put("gold",hgameUserRef.getGold());
-        result.put("boostersCount",hgameUserRef.getBoostersCount());
-        result.put("starsPerLevel",hgameUserRef.getStarsPerLevel());
-        return result;
-    }
-
-    public static HgameUserInfo getUserInfoParent(String parentId) {
-        HgameUserInfo userInfo = new HgameUserInfo();
-        userInfo.setStatus("0");
-        userInfo.setParentId(parentId);
-        return userInfo;
-    }
-
     public static HgameUserRef initGameUserRef(String gameId,String userId){
         HgameUserRef userRef = new HgameUserRef();
         userRef.setGameId(gameId);
@@ -89,27 +92,12 @@ public class DbGameContact {
         userRef.setLevelsCompleted(0L);
         userRef.setTotalScore(0L);
         userRef.setGold(0L);
-        userRef.setBoostersCount("[0,0,0,0,0,0]"); // 骰子:200,定时器:200,闪电:150,丘比特:250,太阳:200
+        // 骰子:200,定时器:200,闪电:150,丘比特:250,太阳:200
+        userRef.setBoostersCount("[0,0,0,0,0,0]");
         userRef.setStarsPerLevel("[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]");
         return userRef;
     }
-
-    public static HgameUserRef getGameUserRefUserId(String userId) {
-        HgameUserRef userRef = new HgameUserRef();
-        userRef.setUserId(userId);
-        userRef.setStatus("0");
-        return userRef;
-    }
-
-    public static HgameUserRef getGameUserRefUser(String userId,String gameId) {
-        HgameUserRef userRef = new HgameUserRef();
-        userRef.setGameId(gameId);
-        userRef.setUserId(userId);
-        userRef.setStatus("0");
-        return userRef;
-    }
-
-    public static HgamePlayRecord initRecord(String userId,String gameId,String playId,int type,Long level,Long gold,Long score,Long start,String tag){
+    public static HgamePlayRecord initGamePlayRecord(String userId,String gameId,String playId,int type,Long level,Long gold,Long score,Long start,String tag){
         HgamePlayRecord record = new HgamePlayRecord();
         record.setStart(0L);
         record.setLbeans(0L);
@@ -126,24 +114,8 @@ public class DbGameContact {
         return record;
     }
 
-    public static HgamePlayRecord uniqueRecord(int type,String userId,String gameId,Long level){
-        HgamePlayRecord record = new HgamePlayRecord();
-        record.setStatus("0");
-        record.setType(type);
-        record.setUserId(userId);
-        record.setGameId(gameId);
-        record.setLevel(level);
-        return record;
-    }
 
-    public static HgameUserRef updateGameUserRef(String userId,String gameId,Long level,Long score,long oldTotalScore){
-        HgameUserRef userRef = new HgameUserRef();
-        userRef.setUserId(userId);
-        userRef.setGameId(gameId);
-        userRef.setLevelsCompleted(level);
-        userRef.setTotalScore(score+oldTotalScore);
-        return userRef;
-    }
+
 
     public static HgameUserRef resetGameUserRef(String userId,String gameId,Long oldTotalScore,Long newScore){
         HgameUserRef userRef = new HgameUserRef();
@@ -155,13 +127,56 @@ public class DbGameContact {
         userRef.setTotalScore(oldTotalScore + newScore);
         return userRef;
     }
-
-
-    public static HgameUserRef updateGameUserGold(String userId,String gameId,Long gold){
+    public static HgameUserRef updateGameUserRefGold(String userId,String gameId,Long gold){
         HgameUserRef userRef = new HgameUserRef();
         userRef.setUserId(userId);
         userRef.setGameId(gameId);
         userRef.setGold(gold);
+        return userRef;
+    }
+    public static HgameUserRef updateGameUserRef(String userId,String gameId,Long level,Long score,long oldTotalScore){
+        HgameUserRef userRef = new HgameUserRef();
+        userRef.setUserId(userId);
+        userRef.setGameId(gameId);
+        userRef.setLevelsCompleted(level);
+        userRef.setTotalScore(score+oldTotalScore);
+        return userRef;
+    }
+
+
+
+    public static HgameInfo paramsGameInfo(Integer type) {
+        HgameInfo hgameInfo = new HgameInfo();
+        hgameInfo.setStatus("0");
+        hgameInfo.setType(type);
+        return hgameInfo;
+    }
+    public static HgameUserInfo paramsGameUserInfo(String parentId) {
+        HgameUserInfo userInfo = new HgameUserInfo();
+        userInfo.setStatus("0");
+        userInfo.setParentId(parentId);
+        return userInfo;
+    }
+    public static HgamePlayRecord paramsGamePlayRecord(int type,String userId,String gameId,Long level){
+        HgamePlayRecord record = new HgamePlayRecord();
+        record.setStatus("0");
+        record.setType(type);
+        record.setUserId(userId);
+        record.setGameId(gameId);
+        record.setLevel(level);
+        return record;
+    }
+    public static HgameUserRef paramsGameUserRef(String userId) {
+        HgameUserRef userRef = new HgameUserRef();
+        userRef.setUserId(userId);
+        userRef.setStatus("0");
+        return userRef;
+    }
+    public static HgameUserRef paramsGameUserRef(String userId,String gameId) {
+        HgameUserRef userRef = new HgameUserRef();
+        userRef.setGameId(gameId);
+        userRef.setUserId(userId);
+        userRef.setStatus("0");
         return userRef;
     }
 }
