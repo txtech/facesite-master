d080144653035d07d814eee7c4928085a976dc51
lihai_1107@163.com <lihai1107>
2021-05-26 11:02:03 +0800

    增加nabob模块,修改数据库

--- modules/nabob/src/main/java/com/nabobsite/modules/nabob/api/dao/CustomUserInfoDao.java
+++ modules/nabob/src/main/java/com/nabobsite/modules/nabob/api/dao/CustomUserInfoDao.java
@@ -1,5 +1,6 @@
 package com.nabobsite.modules.nabob.api.dao;
 
+import com.nabobsite.modules.nabob.cms.team.entity.TeamUser;
 import com.nabobsite.modules.nabob.cms.user.dao.UserInfoDao;
 import com.nabobsite.modules.nabob.cms.user.entity.UserInfo;
 
@@ -10,7 +11,7 @@ public interface CustomUserInfoDao  {
      * @author nada
      * @create 2021/5/14 2:49 下午
      */
-    long updateTeamNum(UserInfo userInfo);
+    long updateTeamNum(TeamUser teamUser);
 
     /**
      * @desc 获取达到等级的直推团队人数
